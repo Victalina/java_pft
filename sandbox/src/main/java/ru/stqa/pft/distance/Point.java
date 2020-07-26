f5f0127a7bdcb830b130fed6722fc89499f041ad
Victalina <60832297+Victalina@users.noreply.github.com>
2020-07-27 01:44:57 +0300

    Задание 2, пункт 4

--- sandbox/src/main/java/ru/stqa/pft/distance/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/distance/Point.java
@@ -8,4 +8,8 @@ public class Point {
     this.x = x;
     this.y = y;
   }
+  public double distance(Point r2){
+
+    return Math.sqrt((r2.x-this.x)*(r2.x-this.x) + (r2.y-this.y)*(r2.y-this.y));
+  }
 }
