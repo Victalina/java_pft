9754082a13627a8efea076a6bcf7940c7f081cdf
Victalina <60832297+Victalina@users.noreply.github.com>
2020-08-02 21:18:56 +0300

    Добавлены тесты для добавления контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String lastName;
+  private final String address;
+  private final String phoneMobile;
+  private final String email;
+
+  public ContactData(String firstName, String lastName, String address, String phoneMobile, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.phoneMobile = phoneMobile;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhoneMobile() {
+    return phoneMobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
